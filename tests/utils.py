82cfc20abfe03ae5531d3e6180a037871b591303
Gregory Poole <gbpoole@gmail.com>
2023-08-28 21:59:08 +1000

    Refactor tests and add .gitignore

---
+++ tests/utils.py
@@ -0,0 +1,57 @@
+from contextlib import contextmanager
+import shlex
+import os
+import subprocess
+from cookiecutter.utils import rmtree
+
+
+@contextmanager
+def inside_dir(dirpath):
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    # sys.path.append('/Users/gpoole/my_code/adacs_python_template')
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project_path))
+
+
+def run_inside_dir(command, dirpath):
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param command: Command that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    with inside_dir(dirpath):
+        return subprocess.check_call(shlex.split(command))
+
+
+def check_output_inside_dir(command, dirpath):
+    "Run a command from inside a given directory, returning the command output"
+    with inside_dir(dirpath):
+        return subprocess.check_output(shlex.split(command))
+
+
+def project_info(result):
+    """Get toplevel dir, project_slug, and project dir from baked cookies"""
+    project_slug = result.project_path.name
+    project_dir = result.project_path / project_slug
+    return str(result.project_path), str(project_slug), str(project_dir)
