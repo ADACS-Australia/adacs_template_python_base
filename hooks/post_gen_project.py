e3d7861cc6fab6e5579f53c2f0ddfab576274c12
Gregory Poole <gbpoole@gmail.com>
2023-08-31 12:43:10 +1000

    Some debugging of instructions printing.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,41 @@
 import subprocess
 
+# Rich is a cookiecutter dependency, so these should be safe here
+from rich.console import Console
+from rich.markdown import Markdown
+
 
 def git():
     subprocess.call(["git", "init"])
     subprocess.call(["git", "add", "*"])
+    subprocess.call(
+        [
+            "git",
+            "remote",
+            "add",
+            "origin",
+            "https://github.com/{{cookiecutter.github_login}}/{{cookiecutter.__project_slug}}.git",
+        ]
+    )
+
     subprocess.call(["git", "commit", "-m", "Initial commit of template code"])
 
 
 def print_instructions():
-    with open("README.1st.config_services", "r") as file:
-        lines = []
+
+    # Read the instructions file, ignoring lines that start with '***'
+    with open("INSTRUCTIONS.template", "r") as file:
+        lines = ""
         line_in = file.readline()
         while line_in:
             if line_in[0:3] != "***":
-                lines.append(line_in.rstrip())
+                lines = lines + line_in
             line_in = file.readline()
+
+    # Render the file as Markdown with Rich
+    print()
+    console = Console()
+    console.print(Markdown(lines))
 
 
 if __name__ == "__main__":
