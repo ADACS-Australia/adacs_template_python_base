598de80e3337473a7b3b8293ee3261eccd2928af
Gregory Poole <gbpoole@gmail.com>
2023-08-31 12:19:52 +1000

    Print instructions after template is rendered.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,5 +7,16 @@ def git():
     subprocess.call(["git", "commit", "-m", "Initial commit of template code"])
 
 
+def print_instructions():
+    with open("README.1st.config_services", "r") as file:
+        lines = []
+        line_in = file.readline()
+        while line_in:
+            if line_in[0:3] != "***":
+                lines.append(line_in.rstrip())
+            line_in = file.readline()
+
+
 if __name__ == "__main__":
     git()
+    print_instructions()
