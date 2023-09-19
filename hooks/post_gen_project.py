83f1448dc053203d9b27c238515a458258e9baf8
Gregory Poole <gbpoole@gmail.com>
2023-09-19 11:21:57 +1000

    Post render instructions complete.  venv implementation incomplete.  Tests failing.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,26 +18,42 @@ def git():
         ]
     )
     subprocess.call(["git", "commit", "-m", "Initial commit of template code"])
-    subprocess.call(["git", "tag",          "v0.0.0"])
+    subprocess.call(["git", "tag", "v0.0.0"])
+
+
+def venv():
+    pass
+    # Check if PYENV_VERSION is defined and if so, that it's set to system?
+    # subprocess.call(["poetry", "config", "--local", "virtualenvs.in-project", "true"])
+    # subprocess.call(["poetry", "config", "--local", "virtualenvs.create", "true"])
+
+
+def install():
+    pass
+    # subprocess.call(["poetry","install","--all-extras"])
 
 
 def print_instructions():
 
     # Read the instructions file, ignoring lines that start with '***'
     with open("INSTRUCTIONS.template", "r") as file:
-        lines = ""
+
+        # Add a notification that the rendering is complete to the instructions being written
+        lines = "# Rendering Complete"
+
         line_in = file.readline()
         while line_in:
-            if line_in[0:3] != "***":
+            if line_in[0:2] != "//":
                 lines = lines + line_in
             line_in = file.readline()
 
     # Render the file as Markdown with Rich
-    print()
     console = Console()
     console.print(Markdown(lines))
 
 
 if __name__ == "__main__":
     git()
+    venv()
+    install()
     print_instructions()
